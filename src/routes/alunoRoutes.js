d2eaf9d7d75c661d589f531b7fc16e339fd39954
Leonardo <leocasttro01@icloud.com>
2022-09-09 18:00:33 -0300

    Seguranca reforçada com solicitacao de login

--- src/routes/alunoRoutes.js
+++ src/routes/alunoRoutes.js
@@ -1,12 +1,13 @@
 import { Router } from 'express';
 import AlunoController from '../controllers/AlunoControllers';
+import loginRequeried from '../middlewares/loginRequeried';
 
 const router = new Router();
 
 router.get('/', AlunoController.index);
-router.post('/', AlunoController.store);
-router.put('/:id', AlunoController.update);
+router.post('/', loginRequeried, AlunoController.store);
+router.put('/:id', loginRequeried, AlunoController.update);
 router.get('/:id', AlunoController.show);
-router.delete('/:id', AlunoController.delete);
+router.delete('/:id', loginRequeried, AlunoController.delete);
 
 export default router;
